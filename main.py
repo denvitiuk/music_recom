822e6e13c390e1e5976b5bef8e819c966c1348e1
denisvituk <denvittiuk@gmail.com>
2024-08-19 15:48:37 +0200

    second

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-# This is a sample Python script.
+"""This module features functions and classes to manipulate data for the
+collaborative filtering algorithm.
+"""
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from pathlib import Path
+
+import scipy
+import pandas as pd
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def load_user_artists(user_artists_file: Path) -> scipy.sparse.csr_matrix:
+    """Load the user artists file and return a user-artists matrix in csr
+    fromat.
+    """
+    user_artists = pd.read_csv(user_artists_file, sep="\t")
+    user_artists.set_index(["userID", "artistID"], inplace=True)
+    coo = scipy.sparse.coo_matrix(
+        (
+            user_artists.weight.astype(float),
+            (
+                user_artists.index.get_level_values(0),
+                user_artists.index.get_level_values(1),
+            ),
+        )
+    )
+    return coo.tocsr()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class ArtistRetriever:
+    """The ArtistRetriever class gets the artist name from the artist ID."""
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def __init__(self):
+        self._artists_df = None
+
+    def get_artist_name_from_id(self, artist_id: int) -> str:
+        """Return the artist name from the artist ID."""
+        return self._artists_df.loc[artist_id, "name"]
+
+    def load_artists(self, artists_file: Path) -> None:
+        """Load the artists file and stores it as a Pandas dataframe in a
+        private attribute.
+        """
+        artists_df = pd.read_csv(artists_file, sep="\t")
+        artists_df = artists_df.set_index("id")
+        self._artists_df = artists_df
+
+
+if __name__ == "__main__":
+    # user_artists_matrix = load_user_artists(
+    #     Path("../lastfmdata/user_artists.dat")
+    # )
+    # print(user_artists_matrix)
+
+    artist_retriever = ArtistRetriever()
+    artist_retriever.load_artists(Path("artists.dat"))
+    artist = artist_retriever.get_artist_name_from_id(3)
+    print(artist)
